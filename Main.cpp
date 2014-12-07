e796cbb76f190ebf27fc9d9b0f83f13972a590b8
fireland15 <fireland15@users.noreply.github.com>
2014-12-06 20:31:49 -0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -10,46 +10,31 @@
 
 #include "NW_MultiAlign.h"
 #include "Multi_Sequence.h"
+#include "NJ.h"
 
 using namespace std;
 
 int main() {
-	MultiSequence* MS1 = new MultiSequence();
-	MultiSequence* MS2 = new MultiSequence();
-	MultiSequence* MS3 = new MultiSequence();
-	MultiSequence* MS4 = new MultiSequence();
+	vector<MultiSequence*>* SeqSet;
+	SeqSet = new vector<MultiSequence*>;
 
-	Sequence S1, S2, S3, S4;
-	S1.setSequenceFromTextFile("seq1.txt");
-	S2.setSequenceFromTextFile("seq2.txt");
-	S3.setSequenceFromTextFile("seq3.txt");
-	S4.setSequenceFromTextFile("seq4.txt");
+	ifstream input;
+	input.open("InputSequences.txt");
+	int i = 0;
+	while (!input.eof()) {
+		string str;
+		getline(input, str);
+		Sequence seq;
+		seq.setSequence(str);
+		seq.setAccessionNum(i);
+		SeqSet->push_back(new MultiSequence(seq));
+		i++;
+	}
+	input.close();
 
-	MS1->setFirstSequence(S1);
-	MS2->setFirstSequence(S2);
-	MS3->setFirstSequence(S3);
-	MS4->setFirstSequence(S4);
-	
-    NWMultiAlign A1, A2, A3;
-	
-    A1.SetMultiSequence(MS1, 1);
-    A1.SetMultiSequence(MS2, 2);
-	
-	A2.SetMultiSequence(MS3, 1);
-	A2.SetMultiSequence(MS4, 2);
-	
-	A1.AlignMultiSequences();
-	
-	A2.AlignMultiSequences();
-    
-	A3.SetMultiSequence(A1.GetAlignedMultiSequence(), 1);
-	A3.SetMultiSequence(A2.GetAlignedMultiSequence(), 2);
-	
-	A3.AlignMultiSequences();
-    
-	A1.WriteAlignedMultiSequenceToFile("A1.txt");
-	A2.WriteAlignedMultiSequenceToFile("A2.txt");
-	A3.WriteAlignedMultiSequenceToFile("A3.txt");
-	
+	NeighborJoin(SeqSet);
+
+	(*SeqSet)[0]->WriteMultiSequenceToFile("ALIGNEDSEQUENCESWEGNIURNU.txt");
+
 	return 0;
 }
