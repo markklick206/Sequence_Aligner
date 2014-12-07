57ed2ddb3fba6ff1f2b8a0aca53ba23b9a9469a2
fireland15 <fireland15@users.noreply.github.com>
2014-12-06 20:32:04 -0800

    Update NW_MultiAlign.h

--- NW_MultiAlign.h
+++ NW_MultiAlign.h
@@ -26,11 +26,15 @@ public:
   
   bool WriteAlignedMultiSequenceToFile(std::string filename);
   void OutputTraceAndScoringMatrices(std::string filename);
-  
+
+  double alignmentScore;
+  double SequenceDistance();
+  int LevenshteinDistance();
+
 private:
 // Variables
   MultiSequence *MS1, *MS2, *MSOut;
-  double alignmentScore;
+  
 	int MATCH = 2;
 	int MISMATCH = -1;
 	int GAP = -2;
